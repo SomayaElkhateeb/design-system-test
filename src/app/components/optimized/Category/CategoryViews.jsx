02394d48a98925a9063ec17466ecb95f23b2a96d
DreamEcho100 <maze6572198@gmail.com>
2024-04-01 14:36:15 +0200

    feat: refactoring 'CategoryViews' component

--- src/app/components/optimized/Category/CategoryViews.jsx
+++ src/app/components/optimized/Category/CategoryViews.jsx
@@ -1,65 +1,67 @@
-import { MoreIcon } from "src/app/utils/icons";
+// Done refactoring to type
+import { MoreIcon } from 'src/app/utils/icons';
 
 /**
  * @param {object} props - Props for the CategoryViews component
  * @param {string} props.imageUrl - The URL of the image for the category
  * @param {string} props.title - The title of the category
- * @param {string} props.description - The description of the category
- * @param {boolean} props.hasAction - Determines whether the category has an action button
+ * @param {string | undefined} props.description - The description of the category
+ * @param {boolean | undefined} props.hasAction - Determines whether the category has an action button
+ *
+ * @description
+ *
+ * Usage Example:
+ *
+ * ```jsx
+ * import React from "react";
+ * import { CategoryViews } from "./CategoryViews";
+ *
+ * export default function MyComponent() {
+ *   return (
+ *     <CategoryViews
+ *       imageUrl="/path/to/image.jpg"
+ *       title="Category Title"
+ *       description="Category Description"
+ *       hasAction={true}
+ *     />
+ *   );
+ * };
+ * ```
  */
+export default function CategoryViews(props) {
+	// Determine the size class for the image based on whether description is present
+	const imageSizeClass = props.description ? 'size-15' : 'size-11';
 
-const CategoryViews = ({ imageUrl, title, description, hasAction }) => {
-  // Determine the size class for the image based on whether description is present
-  const imageSizeClass = description ? "size-[60px]" : "size-[43px]";
-
-  return (
-    <div className="bg-white flex justify-between items-center p-4">
-      <div className="flex">
-        {/* Category Image */}
-        <div className={`rounded-lg border ${imageSizeClass}`}>
-          <img src={imageUrl} alt={title} className="size-full object-cover" />
-        </div>
-        {/* Category Title and Description */}
-        <div className="ml-1 gap-4 flex flex-col justify-center">
-          <h2 className="title">{title}</h2>
-          {description && (
-            <p className="paragraph text-subtitle">{description}</p>
-          )}
-        </div>
-      </div>
-      {/* Action Button */}
-      {hasAction && (
-        <button className="self-start">
-          <MoreIcon className="fill-subtitle" />
-        </button>
-      )}
-    </div>
-  );
-};
-
-export default CategoryViews;
+	return (
+		<div className='flex items-center justify-between p-4 bg-white'>
+			<div className='flex'>
+				{/* Category Image */}
+				<div className={`rounded-lg border ${imageSizeClass}`}>
+					<img
+						src={props.imageUrl}
+						alt={props.title}
+						className='object-cover size-full'
+					/>
+				</div>
+				{/* Category Title and Description */}
+				<div className='flex flex-col justify-center gap-4 ml-1'>
+					<h2 className='title'>{props.title}</h2>
+					{props.description && (
+						<p className='paragraph text-subtitle'>{props.description}</p>
+					)}
+				</div>
+			</div>
+			{/* Action Button */}
+			{props.hasAction && (
+				<button className='self-start'>
+					<MoreIcon className='fill-subtitle' />
+				</button>
+			)}
+		</div>
+	);
+}
 
 // Default props
 CategoryViews.defaultProps = {
-  imageUrl: "", // Provide the URL of the image
-  title: "Category Title", // Default category title
-  description: "", // Default category description
-  hasAction: false, // Default to no action button
+	title: 'Category Title' // Default category title
 };
-
-// Usage Example:
-// import React from "react";
-// import { CategoryViews } from "./CategoryViews";
-//
-// const MyComponent = () => {
-//   return (
-//     <CategoryViews
-//       imageUrl="/path/to/image.jpg"
-//       title="Category Title"
-//       description="Category Description"
-//       hasAction={true}
-//     />
-//   );
-// };
-//
-// export default MyComponent;
