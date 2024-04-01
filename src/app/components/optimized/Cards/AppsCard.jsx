87373d475cd9fa71813a8e10f46dab31ec9e7ad8
DreamEcho100 <maze6572198@gmail.com>
2024-04-01 12:39:08 +0200

    feat: refactoring 'AppsCard' component

--- src/app/components/optimized/Cards/AppsCard.jsx
+++ src/app/components/optimized/Cards/AppsCard.jsx
@@ -1,36 +1,36 @@
-import { FaTelegram } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { getImageUrl } from "src/app/utils";
+// Done refactoring to type
+import { Link } from 'react-router-dom';
+import { getImageUrl } from 'src/app/utils';
 
-const AppsCard = ({ image, name, description, status, url }) => {
-  const statusPadge = status === "available" ? "free" : "installed";
+/**
+ * @param {object} props
+ * @param {string} props.id
+ * @param {string} props.name
+ * @param {string} props.description
+ * @param {string} props.image
+ * @param {string} props.status
+ * @param {string} props.url
+ */
+export default function AppsCard(props) {
+	const statusBadge = props.status === 'available' ? 'free' : 'installed';
 
-  return (
-    <Link
-      to={url}
-      className="bg-white rounded-lg shadow-md p-3 flex gap-3  border border-border-color"
-    >
-      <div className="size-[60px] grid place-content-center min-w-[60px] rounded-lg border border-light-2 overflow-hidden">
-        <img src={image} className="w-full object-cover" />
-      </div>
-      <div>
-        <h3 className="title mb-2">{name}</h3>
-        <p className="paragraph text-subtitle">{description}</p>
-        <img
-          src={getImageUrl(`$padges/${statusPadge}.svg`)}
-          alt="status"
-          className="h-7 mt-3"
-        />
-      </div>
-    </Link>
-  );
-};
-export default AppsCard;
-
-AppsCard.defaultProps = {
-  icon: <FaTelegram size={48} color="#2EA6DA" />,
-  heading: "Telegram",
-  paragraph:
-    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum soluta,eaque ducimus perspiciatis odio repudiandae nisi cupiditate doloribus",
-  label: "free",
-};
+	return (
+		<Link
+			to={props.url}
+			className='flex gap-3 p-3 bg-white border rounded-lg shadow-md border-border-color'
+		>
+			<div className='size-[60px] grid place-content-center min-w-[60px] rounded-lg border border-light-2 overflow-hidden'>
+				<img src={props.image} className='object-cover w-full' />
+			</div>
+			<div>
+				<h3 className='mb-2 title'>{props.name}</h3>
+				<p className='paragraph text-subtitle'>{props.description}</p>
+				<img
+					src={getImageUrl(`$padges/${statusBadge}.svg`)}
+					alt='status'
+					className='mt-3 h-7'
+				/>
+			</div>
+		</Link>
+	);
+}
