5fdeee9146714dd91e22afb35ec792168ef4676c
DreamEcho100 <maze6572198@gmail.com>
2024-04-02 13:10:22 +0200

    feat: refactoring 'SingleChoiceChips' components

--- src/app/components/optimized/ChoiceChips/SingleChoiceChips.jsx
+++ src/app/components/optimized/ChoiceChips/SingleChoiceChips.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react';
-
-/**
- * @typedef {string | { value: string; key: string; }} Option
- */
+/** @typedef {string} Option */
 
 /**
  * @param {object} props
  * @param {Option[]} props.options An array of options to display as chips
- * @param {((option: string) => void) | (option: { value: string; key: string; }) => void} props.setOption Function to set the selected option
+ * @param {((option: string) => void)} props.setSelected Function to set the selected option
+ * @param {Option} props.selected The selected option
  * @param {import('react').ReactNode} props.icon Optional icon component to display with each chip
- * @param {string} props.type Type of options: 'array' or 'object'
+ * @param {string | undefined} props.type Type of options: 'array' or 'object'
  *
  * @description
  *
@@ -40,15 +37,15 @@ import { useState } from 'react';
  *   return (
  *     <div>
  *       <SingleChoiceChips
- *         options={simpleOptions}
- *         setOption={handleOptionSelect}
+ * 			   options={keyValueOptions}
+ * 			   setSelected={handleOptionSelect}
+ * 			   selected={selectedOption}
  *         icon={<LocationIcon />} // Optional icon
- *         type="simple"
  *       />
  *       <SingleChoiceChips
- *         options={keyValueOptions}
- *         setOption={handleOptionSelect}
- *         type="keyValue"
+ * 			   options={keyValueOptions}
+ * 			   setSelected={handleOptionSelect}
+ * 			   selected={selectedOption}
  *       />
  *    </div>
  *   );
@@ -58,33 +55,17 @@ import { useState } from 'react';
  * ```
  */
 export default function SingleChoiceChips(props) {
-	const [selectedOption, setSelectedOption] = useState('');
-
-	/**
-	 * @param {Option} option
-	 */
-	const handleOptionSelect = (option) => {
-		setSelectedOption(option);
-		props.setOption(option);
-	};
-
 	return (
 		<div>
-			<div className='flex gap-2'>
+			<div className='flex gap-2 p-2'>
+				{/* Render each option as a chip */}
 				{props.options.map((option, index) => (
 					<Chip
 						key={index}
-						label={props.type === 'array' ? option : option.key}
-						value={props.type === 'array' ? option : option.value}
-						isSelected={
-							props.type === 'array'
-								? option === selectedOption
-								: option.value === selectedOption
-						}
-						// label={type === 'array' ? option : option.key}
-						// isSelected={type === 'array' ? option === selectedOption : option.value === selectedOption}
+						isSelected={option === props.selected}
 						icon={props.icon}
-						onSelect={handleOptionSelect}
+						onSelect={props.setSelected}
+						value={option}
 					/>
 				))}
 			</div>
@@ -94,13 +75,13 @@ export default function SingleChoiceChips(props) {
 
 /**
  * @param {object} props
- * @param {string} props.label The text label for the chip
- * @param {} props.value The value associated with the chip
+ * @param {string} props.value The value associated with the chip
  * @param {boolean} props.isSelected Boolean indicating whether the chip is selected
  * @param {import('react').ReactNode} props.icon Optional icon component to display with the chip
- * @param {} props.onSelect Function to handle chip selection
+ * @param {((option: string) => void)} props.onSelect Function to handle chip selection
  */
 function Chip(props) {
+	// Base styles for the chip
 	const baseStyle =
 		'flex items-center border p-1 pr-2 w-fit rounded-full cursor-pointer transition-all';
 	const notSelectedStyle =
@@ -120,8 +101,13 @@ function Chip(props) {
 					props.isSelected ? 'text-white' : 'text-subtitle'
 				} `}
 			>
-				{props.label}
+				{props.value}
 			</span>
 		</div>
 	);
 }
+
+SingleChoiceChips.defaultProps = {
+	icon: null,
+	type: 'array'
+};
--- src/pages/MarketingPage/Discounts/NewDiscount/BasicInfo/comp/ApplyToOptions.jsx
+++ src/pages/MarketingPage/Discounts/NewDiscount/BasicInfo/comp/ApplyToOptions.jsx
@@ -1,139 +1,140 @@
-import { Button, InputRow, SelectItems } from "src/app/components/optimized";
-import { FaChevronRight } from "react-icons/fa";
-import { AiOutlinePercentage } from "react-icons/ai";
+import { Button, InputRow, SelectItems } from 'src/app/components/optimized';
+import { FaChevronRight } from 'react-icons/fa';
+import { AiOutlinePercentage } from 'react-icons/ai';
 
-import useDiscountForm from "../useDiscountForm";
-import { selectCategories, selectProducts, customerGetsOptions } from "./data";
-import SingleChoiceChips from "src/app/components/optimized/ChoiceChips/SingleChoiceChips";
+import useDiscountForm from '../useDiscountForm';
+import { selectCategories, selectProducts, customerGetsOptions } from './data';
+import SingleChoiceChips from 'src/app/components/optimized/ChoiceChips/SingleChoiceChips';
 const ApplyToOptions = ({ applyTo }) => {
-  const { showSelectCategories, customerGets, percentGets, quantityGets } =
-    useDiscountForm();
-  console.log("showSelectCategories", showSelectCategories);
-  console.log("customerGets", customerGets);
-  console.log("percentGets", percentGets);
-  console.log("quantityGets", quantityGets);
+	const { showSelectCategories, customerGets, percentGets, quantityGets } =
+		useDiscountForm();
+	console.log('showSelectCategories', showSelectCategories);
+	console.log('customerGets', customerGets);
+	console.log('percentGets', percentGets);
+	console.log('quantityGets', quantityGets);
 
-  return (
-    <div>
-      {applyTo === "All products" && (
-        <h1 className="mt-[18px]">All products</h1>
-      )}
-      {applyTo === "Specific category" && <SpecificCategory />}
-      {applyTo === "Specific products" && <SpecificProducts />}
-      {applyTo === "Buy x get y" && <BuyxGety />}
-    </div>
-  );
+	return (
+		<div>
+			{applyTo === 'All products' && (
+				<h1 className='mt-[18px]'>All products</h1>
+			)}
+			{applyTo === 'Specific category' && <SpecificCategory />}
+			{applyTo === 'Specific products' && <SpecificProducts />}
+			{applyTo === 'Buy x get y' && <BuyxGety />}
+		</div>
+	);
 };
 
 export default ApplyToOptions;
 
 // applyTo components
 const SpecificCategory = () => {
-  const {
-    showSelectCategories,
-    setShowSelectCategories,
-    handleCloseSelectCategories,
-  } = useDiscountForm();
-  return (
-    <div className="mt-[18px] flex flex-col gap-[18px]">
-      <div>
-        <Button
-          variant="sec"
-          text="select categories"
-          RightIcon={FaChevronRight}
-          onClick={setShowSelectCategories}
-        />
-      </div>
-      {showSelectCategories && (
-        <SelectItems
-          onClose={handleCloseSelectCategories}
-          select={selectCategories}
-          title="Select categories"
-        />
-      )}
+	const {
+		showSelectCategories,
+		setShowSelectCategories,
+		handleCloseSelectCategories
+	} = useDiscountForm();
+	return (
+		<div className='mt-[18px] flex flex-col gap-[18px]'>
+			<div>
+				<Button
+					variant='sec'
+					text='select categories'
+					RightIcon={FaChevronRight}
+					onClick={setShowSelectCategories}
+				/>
+			</div>
+			{showSelectCategories && (
+				<SelectItems
+					onClose={handleCloseSelectCategories}
+					select={selectCategories}
+					title='Select categories'
+				/>
+			)}
 
-      {/* get data from local storage */}
-    </div>
-  );
+			{/* get data from local storage */}
+		</div>
+	);
 };
 
 const SpecificProducts = () => {
-  const {
-    showSelectCategories,
-    setShowSelectCategories,
-    handleCloseSelectCategories,
-  } = useDiscountForm();
-  return (
-    <div className="mt-[18px] flex flex-col gap-[18px]">
-      <div>
-        <Button
-          variant="sec"
-          text="select products"
-          RightIcon={FaChevronRight}
-          onClick={setShowSelectCategories}
-        />
-      </div>
-      {showSelectCategories && (
-        <SelectItems
-          onClose={handleCloseSelectCategories}
-          select={selectProducts}
-          title="Select products"
-        />
-      )}
-    </div>
-  );
+	const {
+		showSelectCategories,
+		setShowSelectCategories,
+		handleCloseSelectCategories
+	} = useDiscountForm();
+	return (
+		<div className='mt-[18px] flex flex-col gap-[18px]'>
+			<div>
+				<Button
+					variant='sec'
+					text='select products'
+					RightIcon={FaChevronRight}
+					onClick={setShowSelectCategories}
+				/>
+			</div>
+			{showSelectCategories && (
+				<SelectItems
+					onClose={handleCloseSelectCategories}
+					select={selectProducts}
+					title='Select products'
+				/>
+			)}
+		</div>
+	);
 };
 
 const BuyxGety = () => {
-  const {
-    setCustomerGets,
-    customerGets,
-    percentGets,
-    setPercentGets,
-    quantityGets,
-    setQuantityGets,
-  } = useDiscountForm();
-  return (
-    <div className="mt-[18px] flex flex-col gap-[18px]">
-      <div>
-        <Button
-          variant="sec"
-          text="select products X"
-          RightIcon={FaChevronRight}
-        />
-      </div>
-      <p>Customer Gets (5 products Y with 20% offer)</p>
+	const {
+		setCustomerGets,
+		customerGets,
+		percentGets,
+		setPercentGets,
+		quantityGets,
+		setQuantityGets
+	} = useDiscountForm();
+	return (
+		<div className='mt-[18px] flex flex-col gap-[18px]'>
+			<div>
+				<Button
+					variant='sec'
+					text='select products X'
+					RightIcon={FaChevronRight}
+				/>
+			</div>
+			<p>Customer Gets (5 products Y with 20% offer)</p>
 
-      <SingleChoiceChips
-        options={customerGetsOptions}
-        setOption={setCustomerGets}
-      />
-      {customerGets === "Specific customers" && (
-        <>
-          <div className="w-[390px]">
-            <InputRow
-              value={percentGets}
-              onChange={setPercentGets}
-              label="Percentage"
-              leftIcon={<AiOutlinePercentage />}
-            />
-          </div>
-          <div className="w-[390px]">
-            <InputRow
-              value={quantityGets}
-              onChange={setQuantityGets}
-              label="Quantity"
-            />
-          </div>
-          <div>
-            <Button
-              variant="sec"
-              text="select products y"
-              RightIcon={FaChevronRight}
-            />
-          </div>
-        </>
-      )}
-    </div>
-  );
+			<SingleChoiceChips
+				options={customerGetsOptions}
+				setSelected={setCustomerGets}
+				selected={customerGets}
+			/>
+			{customerGets === 'Specific customers' && (
+				<>
+					<div className='w-[390px]'>
+						<InputRow
+							value={percentGets}
+							onChange={setPercentGets}
+							label='Percentage'
+							leftIcon={<AiOutlinePercentage />}
+						/>
+					</div>
+					<div className='w-[390px]'>
+						<InputRow
+							value={quantityGets}
+							onChange={setQuantityGets}
+							label='Quantity'
+						/>
+					</div>
+					<div>
+						<Button
+							variant='sec'
+							text='select products y'
+							RightIcon={FaChevronRight}
+						/>
+					</div>
+				</>
+			)}
+		</div>
+	);
 };
