73c751fddd3c8e7c824907a35627759f9f052220
DreamEcho100 <maze6572198@gmail.com>
2024-04-03 14:12:24 +0200

    feat: refactoring 'PaymentMethodAccordion' component

--- src/app/components/optimized/Payment/PaymentMethodAccordion.jsx
+++ src/app/components/optimized/Payment/PaymentMethodAccordion.jsx
@@ -1,35 +1,24 @@
-import { DownIcon } from "src/app/utils/icons";
-const PaymentMethodAccordion = ({ title, isOpen, onToggle, children }) => {
-  return (
-    <div
-      className={`border border-border-color rounded mb-4  ${
-        isOpen ? "bg-light-1" : "bg-white"
-      }`}
-    >
-      <button
-        type="button"
-        className="w-full p-4 flex justify-between items-center text-left focus:outline-none"
-        onClick={onToggle}
-        // aria-expanded={isOpen}
-      >
-        <h2 className="title">{title}</h2>
-        <DownIcon
-          className={`transition-all fill-hint ${
-            isOpen ? "rotate-180" : "bg-white"
-          }`}
-        />
-      </button>
+import { DownIcon } from 'src/app/utils/icons';
+/**
+ * @param {import('react').PropsWithChildren<{
+ *  title: string;
+ *  isOpen: boolean;
+ *  onToggle: () => void;
+ * }>} props
+ */
+export default function PaymentMethodAccordion(props) {
+	return (
+		<div className={`border border-border-color rounded mb-4  ${props.isOpen ? 'bg-light-1' : 'bg-white'}`}>
+			<button
+				type='button'
+				className='flex items-center justify-between w-full p-4 text-left focus:outline-none'
+				onClick={props.onToggle}
+			>
+				<h2 className='title'>{props.title}</h2>
+				<DownIcon className={`transition-all fill-hint ${props.isOpen ? 'rotate-180' : 'bg-white'}`} />
+			</button>
 
-      <div
-        className={`p-4 ${!isOpen && "hidden"}`}
-        // aria-hidden={!isOpen}
-      >
-        {children}
-      </div>
-    </div>
-  );
-};
-
-export default PaymentMethodAccordion;
-// overflow-hidden
-// className={`transition-all duration-700 ${!isOpen ? "h-0 p-0" : "p-4"}`}
+			<div className={`p-4 ${!props.isOpen && 'hidden'}`}>{props.children}</div>
+		</div>
+	);
+}
