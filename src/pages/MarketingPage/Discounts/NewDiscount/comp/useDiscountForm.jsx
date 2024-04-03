904d0c3adf47d1df009b30fc127de06cfea61fe3
DreamEcho100 <maze6572198@gmail.com>
2024-04-03 15:52:17 +0200

    feat: refactoring 'useDiscountForm' hook

--- src/pages/MarketingPage/Discounts/NewDiscount/comp/useDiscountForm.jsx
+++ src/pages/MarketingPage/Discounts/NewDiscount/comp/useDiscountForm.jsx
@@ -1,176 +1,185 @@
-import { useRef, useState } from "react";
-import useLocalStorage from "../comp/useLocalStorage";
+import { useRef, useState } from 'react';
+import useLocalStorage from '../comp/useLocalStorage';
 
-const useDiscountForm = () => {
-  const [discountName, setDiscountName] = useLocalStorage("discountName", "");
-  const discountNameRef = useRef();
-  const [percentage, setPercentage] = useState("");
-  const [fixedAmount, setFixedAmount] = useLocalStorage("fixedAmount", "");
-  const fixedAmountRef = useRef();
-  const [discountType, setDiscountType] = useState("");
-  const [showSelectCategories, setShowSelectCategories] = useState(false);
-  const [applyTo, setApplyTo] = useState("");
-  const [customerGets, setCustomerGets] = useState("");
-  const [percentGets, setPercentGets] = useLocalStorage("percentGets", "");
-  const [quantityGets, setQuantityGets] = useLocalStorage("quantityGets", "");
-  const [selectedProducts, setSelectedProducts] = useState([]);
-  const [selectedItems, setSelectedItems] = useState([]);
+export default function useDiscountForm() {
+	const [discountName, setDiscountName] = useLocalStorage('discountName', '');
+	const discountNameRef = useRef();
+	const [percentage, setPercentage] = useState('');
+	const [fixedAmount, setFixedAmount] = useLocalStorage('fixedAmount', '');
+	const fixedAmountRef = useRef();
+	const [discountType, setDiscountType] = useState('');
+	const [showSelectCategories, setShowSelectCategories] = useState(false);
+	const [applyTo, setApplyTo] = useState('');
+	const [customerGets, setCustomerGets] = useState('');
+	const [percentGets, setPercentGets] = useLocalStorage('percentGets', '');
+	const [quantityGets, setQuantityGets] = useLocalStorage('quantityGets', '');
+	const [selectedProducts, setSelectedProducts] = useState([]);
+	const [selectedItems, setSelectedItems] = useState([]);
 
-  // const [discounts, setDiscounts] = useLocalStorage("discounts", []);
-  // useState category
-  const [stateCategory, setStateCategory] = useState([]);
-  // useState products
-  const [stateProducts, setStateProducts] = useState([]);
-  // useState customer groups
-  const [stateGroups, setStateGroups] = useState([]);
-  // useState customers
-  const [stateCustomers, setStateCustomers] = useState([]);
+	// const [discounts, setDiscounts] = useLocalStorage("discounts", []);
+	// useState category
+	const [stateCategory, setStateCategory] = useState([]);
+	// useState products
+	const [stateProducts, setStateProducts] = useState([]);
+	// useState customer groups
+	const [stateGroups, setStateGroups] = useState([]);
+	// useState customers
+	const [stateCustomers, setStateCustomers] = useState([]);
 
-  // customer Segment
-  const [optionType, setOptionType] = useState("");
-  const [customerSegment, setCustomerSegment] = useState("");
-  const [showGroups, setShowGroups] = useState(false);
-  const [showCustomers, setShowCustomers] = useState(false);
-  const [selectedCustomers, setSelectedCustomers] = useState([]);
+	// customer Segment
+	const [optionType, setOptionType] = useState('');
+	const [customerSegment, setCustomerSegment] = useState('');
+	const [showGroups, setShowGroups] = useState(false);
+	const [showCustomers, setShowCustomers] = useState(false);
+	const [selectedCustomers, setSelectedCustomers] = useState([]);
 
-  // Minimum Requirements
-  const [minimumReq, setMinimumReq] = useState("");
-  const [showSelectButtons, setShowSelectButtons] = useState(false);
-  const [minimumPrice, setMinimumPrice] = useLocalStorage("minimumPrice", "");
-  const [minimumQuantity, setMinimumQuantity] = useLocalStorage(
-    "minimumQuantity",
-    ""
-  );
-  const [endDatePicker, setEndDatePicker] = useState(false);
-  const [endDate, setEndDate] = useLocalStorage("endDate", "");
+	// Minimum Requirements
+	const [minimumReq, setMinimumReq] = useState('');
+	const [showSelectButtons, setShowSelectButtons] = useState(false);
+	const [minimumPrice, setMinimumPrice] = useLocalStorage('minimumPrice', '');
+	const [minimumQuantity, setMinimumQuantity] = useLocalStorage('minimumQuantity', '');
+	const [endDatePicker, setEndDatePicker] = useState(false);
+	const [endDate, setEndDate] = useLocalStorage('endDate', /** @type {import("dayjs").Dayjs | null} */ (null));
 
-  // LOCAL STORAGE
-  // Function to add an item to the array
-  // Function to add an object to the array
-  const addObjectToArray = (object) => {
-    setDiscounts([...discountName, object]);
-  };
-  // Function to clear the array
-  const clearArray = () => {
-    setDiscountName([]);
-  };
-  // //////////////////////
-  const handleDiscountTypeChange = (type) => {
-    setDiscountType(type);
-  };
+	/*
+	// LOCAL STORAGE
+	// Function to add an item to the array
+	// Function to add an object to the array
+	const addObjectToArray = (object) => {
+			setDiscounts([...discountName, object]);
+	};
 
-  const handleApplyToChange = (value) => {
-    setApplyTo(value);
-  };
+	// Function to clear the array
+	const clearArray = () => {
+			setDiscountName([]);
+	};
+	*/
+	// //////////////////////
+	/** @param {string} type */
+	function handleDiscountTypeChange(type) {
+		setDiscountType(type);
+	}
 
-  const handleCloseSelectCategories = () => {
-    setShowSelectCategories(false);
-  };
+	/** @param {string} value */
+	function handleApplyToChange(value) {
+		setApplyTo(value);
+	}
 
-  const handlePercentGetsChange = (value) => {
-    setPercentGets(value);
-  };
+	function handleCloseSelectCategories() {
+		setShowSelectCategories(false);
+	}
 
-  const handleQuantityGetsChange = (value) => {
-    setQuantityGets(value);
-  };
-  const handleCustomerGetsChange = (value) => {
-    setCustomerGets(value);
-  };
+	/** @param {string} value */
+	function handlePercentGetsChange(value) {
+		setPercentGets(value);
+	}
 
-  const handlecustomersChange = (value) => {
-    setCustomerSegment(value);
-  };
-  const handleOptionChange = (type) => {
-    setOptionType(type);
-  };
-  const hideSelectCustomers = () => {
-    setShowCustomers(false);
-  };
-  const hideSelectGroup = () => {
-    setShowGroups(false);
-  };
+	/** @param {string} value */
+	const handleQuantityGetsChange = (value) => {
+		setQuantityGets(value);
+	};
 
-  // Minimum Requirements
-  const handleCheckBoxClick = () => {
-    setShowSelectButtons(!showSelectButtons);
-  };
+	/** @param {string} value */
+	const handleCustomerGetsChange = (value) => {
+		setCustomerGets(value);
+	};
 
-  const handleMinimumChange = (value) => {
-    setMinimumReq(value);
-  };
-  return {
-    discountName,
-    setDiscountName,
-    discountNameRef,
-    discountType,
-    setDiscountType: handleDiscountTypeChange,
-    fixedAmountRef,
-    applyTo,
-    setApplyTo: handleApplyToChange,
-    customerGets,
-    setCustomerGets: handleCustomerGetsChange,
-    selectedProducts,
-    // handleProductSelect,
-    percentage,
-    setPercentage,
-    fixedAmount,
-    setFixedAmount,
-    percentGets,
-    setPercentGets: handlePercentGetsChange,
-    quantityGets,
-    setQuantityGets: handleQuantityGetsChange,
-    showSelectCategories,
-    setShowSelectCategories,
-    handleCloseSelectCategories,
-    selectedItems,
-    setSelectedCustomers,
-    // customer segment
-    optionType,
-    setOptionType: handleOptionChange,
-    customerSegment,
-    setCustomerSegment: handlecustomersChange,
-    selectedCustomers,
+	/** @param {string} value */
+	function handleCustomersChange(value) {
+		setCustomerSegment(value);
+	}
 
-    // handleProductSelect,
-    showGroups,
-    setShowGroups,
-    hideSelectGroup,
-    showCustomers,
-    setShowCustomers,
-    hideSelectCustomers,
-    // Minimum Requirements
-    minimumReq,
-    setMinimumReq: handleMinimumChange,
-    showSelectButtons,
-    setShowSelectButtons: handleCheckBoxClick,
+	/** @param {string} type */
+	const handleOptionChange = (type) => {
+		setOptionType(type);
+	};
 
-    // handle local storage
+	function hideSelectCustomers() {
+		setShowCustomers(false);
+	}
 
-    setSelectedItems,
+	function hideSelectGroup() {
+		setShowGroups(false);
+	}
 
-    // discounts,
-    // setDiscounts: addObjectToArray,
-    // /////////////////////////////
-    stateCategory,
-    setStateCategory,
-    stateProducts,
-    setStateProducts,
-    stateGroups,
-    setStateGroups,
-    stateCustomers,
-    setStateCustomers,
+	// Minimum Requirements
+	function handleCheckBoxClick() {
+		setShowSelectButtons(!showSelectButtons);
+	}
 
-    minimumPrice,
-    setMinimumPrice,
-    minimumQuantity,
-    setMinimumQuantity,
+	/** @param {string} value */
+	function handleMinimumChange(value) {
+		setMinimumReq(value);
+	}
+	return {
+		discountName,
+		setDiscountName,
+		discountNameRef,
+		discountType,
+		setDiscountType: handleDiscountTypeChange,
+		fixedAmountRef,
+		applyTo,
+		setApplyTo: handleApplyToChange,
+		customerGets,
+		setCustomerGets: handleCustomerGetsChange,
+		selectedProducts,
+		// handleProductSelect,
+		percentage,
+		setPercentage,
+		fixedAmount,
+		setFixedAmount,
+		percentGets,
+		setPercentGets: handlePercentGetsChange,
+		quantityGets,
+		setQuantityGets: handleQuantityGetsChange,
+		showSelectCategories,
+		setShowSelectCategories,
+		handleCloseSelectCategories,
+		selectedItems,
+		setSelectedCustomers,
+		// customer segment
+		optionType,
+		setOptionType: handleOptionChange,
+		customerSegment,
+		setCustomerSegment: handleCustomersChange,
+		selectedCustomers,
 
-    endDatePicker,
-    setEndDatePicker,
-    endDate,
-    setEndDate,
-  };
-};
+		// handleProductSelect,
+		showGroups,
+		setShowGroups,
+		hideSelectGroup,
+		showCustomers,
+		setShowCustomers,
+		hideSelectCustomers,
+		// Minimum Requirements
+		minimumReq,
+		setMinimumReq: handleMinimumChange,
+		showSelectButtons,
+		setShowSelectButtons: handleCheckBoxClick,
 
-export default useDiscountForm;
+		// handle local storage
+		setSelectedItems,
+
+		// discounts,
+		// setDiscounts: addObjectToArray,
+		// /////////////////////////////
+		stateCategory,
+		setStateCategory,
+		stateProducts,
+		setStateProducts,
+		stateGroups,
+		setStateGroups,
+		stateCustomers,
+		setStateCustomers,
+
+		minimumPrice,
+		setMinimumPrice,
+		minimumQuantity,
+		setMinimumQuantity,
+
+		endDatePicker,
+		setEndDatePicker,
+		endDate,
+		setEndDate,
+	};
+}
