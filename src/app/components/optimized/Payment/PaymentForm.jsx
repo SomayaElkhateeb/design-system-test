778818939365d7d523f9ef21c829c7139d6387c3
DreamEcho100 <maze6572198@gmail.com>
2024-04-03 14:10:45 +0200

    feat: refactoring 'PaymentForm' component

--- src/app/components/optimized/Payment/PaymentForm.jsx
+++ src/app/components/optimized/Payment/PaymentForm.jsx
@@ -2,17 +2,36 @@ import { TooltipIcon } from 'src/app/utils/icons';
 import { InputRow } from '..';
 import { getImageUrl } from 'src/app/utils';
 
-const PaymentForm = ({ data, onDataChange, errors }) => {
-	const handleInputChange = (fieldName, value) => {
+/**
+ * @param {{
+ *  data: {
+ *   quantity: string;
+ *   expiryDate: string;
+ *   cvv: string;
+ *   };
+ *  onDataChange: (fieldName: string, value: string) => void;
+ *  errors: {
+ *   quantity: string;
+ *   expiryDate: string;
+ *   cvv: string;
+ *  };
+ * }} props
+ */
+export default function PaymentForm({ data, onDataChange, errors }) {
+	/**
+	 * @param {string} fieldName
+	 * @param {string} value
+	 */
+	function handleInputChange(fieldName, value) {
 		onDataChange(fieldName, value); // Call the callback function with field name and value
-	};
+	}
 
 	return (
 		<section>
 			<div className='grid grid-cols-4 gap-6'>
 				<div className='col-span-2'>
 					<InputRow
-						label={QuantityLable}
+						label={QuantityLabel}
 						error={errors.quantity}
 						// value={data.quantity}
 						handleOnChange={(value) => handleInputChange('quantity', value)}
@@ -28,7 +47,7 @@ const PaymentForm = ({ data, onDataChange, errors }) => {
 				</div>
 				<div className='col-span-1'>
 					<InputRow
-						label={CvvLable}
+						label={CvvLabel}
 						error={errors.cvv}
 						// value={data.cvv}
 						handleOnChange={(value) => handleInputChange('cvv', value)}
@@ -37,10 +56,9 @@ const PaymentForm = ({ data, onDataChange, errors }) => {
 			</div>
 		</section>
 	);
-};
-export default PaymentForm;
+}
 
-const CreditTransactions = () => {
+function CreditTransactions() {
 	return (
 		<div className='flex gap-1'>
 			<img src={getImageUrl('companies/mada.svg')} className='w-5 h-4' />
@@ -49,14 +67,16 @@ const CreditTransactions = () => {
 			<img src={getImageUrl('companies/masterCard.svg')} className='w-5 h-4' />
 		</div>
 	);
-};
-const CvvLable = (
+}
+
+const CvvLabel = (
 	<span className='flex'>
 		CVV&nbsp;
 		<TooltipIcon className='fill-secondary' />
 	</span>
 );
-const QuantityLable = (
+
+const QuantityLabel = (
 	<span className='flex justify-between'>
 		<p>Quantity</p>
 		<CreditTransactions />
--- src/app/components/optimized/Payment/usePayment.jsx
+++ src/app/components/optimized/Payment/usePayment.jsx
@@ -1,46 +1,53 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const usePayment = () => {
-  // State
-  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(null);
-  const [agreeToTerms, setAgreeToTerms] = useState(false);
-  const [errors, setErrors] = useState({});
+	// State
+	const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(null);
+	const [agreeToTerms, setAgreeToTerms] = useState(false);
+	const [errors, setErrors] = useState(
+		/**
+		 * @type {{
+		 *   quantity?: string;
+		 *   expiryDate?: string;
+		 *   cvv?: string;
+		 *  }}
+		 */ ({}),
+	);
 
-  const [paymentData, setPaymentData] = useState({
-    quantity: "",
-    expiryDate: "",
-    cvv: "",
-  });
+	const [paymentData, setPaymentData] = useState({
+		quantity: '',
+		expiryDate: '',
+		cvv: '',
+	});
 
-  const paymentDataHandler = (fieldName, enteredValue) => {
-    setPaymentData((prevPaymentData) => ({
-      ...prevPaymentData,
-      [fieldName]: enteredValue,
-    }));
+	const paymentDataHandler = (fieldName, enteredValue) => {
+		setPaymentData((prevPaymentData) => ({
+			...prevPaymentData,
+			[fieldName]: enteredValue,
+		}));
+	};
 
-  };
+	// Select Payment Method
+	const handlePaymentMethodChange = (method) => {
+		setSelectedPaymentMethod(method === selectedPaymentMethod ? null : method);
+		setPaymentData({ quantity: '', expiryDate: '', cvv: '' });
+	};
+	// Agree to the terms to affect the confirmation button
+	const handleTermsCheckbox = () => {
+		setAgreeToTerms(!agreeToTerms);
+	};
 
-  // Select Payment Method
-  const handlePaymentMethodChange = (method) => {
-    setSelectedPaymentMethod(method === selectedPaymentMethod ? null : method);
-    setPaymentData({ quantity: "", expiryDate: "", cvv: "" });
-  };
-  // Agree to the terms to affect the confirmation button
-  const handleTermsCheckbox = () => {
-    setAgreeToTerms(!agreeToTerms);
-  };
-
-  const isButtonDisabled = !agreeToTerms || !selectedPaymentMethod;
-  return {
-    errors,
-    paymentData,
-    agreeToTerms,
-    isButtonDisabled,
-    selectedPaymentMethod,
-    paymentDataHandler,
-    handleTermsCheckbox,
-    handlePaymentMethodChange,
-  };
+	const isButtonDisabled = !agreeToTerms || !selectedPaymentMethod;
+	return {
+		errors,
+		paymentData,
+		agreeToTerms,
+		isButtonDisabled,
+		selectedPaymentMethod,
+		paymentDataHandler,
+		handleTermsCheckbox,
+		handlePaymentMethodChange,
+	};
 };
 
 export default usePayment;
