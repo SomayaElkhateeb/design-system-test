96f219771eca62e14b7941e5d701f9e84ca94428
DreamEcho100 <maze6572198@gmail.com>
2024-04-03 14:17:58 +0200

    feat: minor changes

--- src/pages/MarketingPage/Apps/Apps.jsx
+++ src/pages/MarketingPage/Apps/Apps.jsx
@@ -4,27 +4,13 @@ import MostPopularList from './comp/MostPopularList';
 import SocialAppsWrapper from './comp/SocialAppsWrapper';
 
 const Apps = () => {
-
-
-
-
-
-
 	return (
 		<div className='flex flex-col gap-8 p-5'>
 			<MostPopularList />
 
-			<SocialAppsWrapper
-				socialApps={socialApps}
-				title='Recommended'
-				linkTo='/'
-			/>
+			<SocialAppsWrapper socialApps={socialApps} title='Recommended' linkTo='/' />
 
-			<SocialAppsWrapper
-				socialApps={socialApps}
-				title='Reach more customers'
-				linkTo='/'
-			/>
+			<SocialAppsWrapper socialApps={socialApps} title='Reach more customers' linkTo='/' />
 		</div>
 	);
 };
--- src/pages/MarketingPage/Apps/comp/MostPopularList.jsx
+++ src/pages/MarketingPage/Apps/comp/MostPopularList.jsx
@@ -6,19 +6,19 @@ import { mostPopularApps } from 'src/app/utils/constants';
 
 const MostPopularList = () => {
 	//  hooks
-	const { t } = useTranslation()
+	const { t } = useTranslation();
 	return (
 		<>
 			<div className='mb-5'>
-				<h2 className='title text-lg'>{t("Most popular")}</h2>
+				<h2 className='text-lg title'>{t('Most popular')}</h2>
 				<p className='paragraph text-subtitle'>
-					{t("For more information about setup guide")}{' '}
+					{t('For more information about setup guide')}{' '}
 					<Link className='text-primary' to=''>
-						{t("Learn more")}
+						{t('Learn more')}
 					</Link>
 				</p>
 			</div>
-			<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
+			<div className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
 				{mostPopularApps.map((app) => (
 					<BigAppsCard key={app.id} {...app} />
 				))}
@@ -39,12 +39,8 @@ const BigAppsCard = ({ image, name, description, url, status }) => {
 						<img src={image} alt={name} className='h-[180px]' />
 					</div>
 					<h2 className='mt-4 title text-[16px]'>{name}</h2>
-					<p className='paragraph mt-2'>{description}</p>
-					<img
-						src={getImageUrl(`padges/${statusPadge}.svg`)}
-						alt='status'
-						className='h-7 mt-3'
-					/>
+					<p className='mt-2 paragraph'>{description}</p>
+					<img src={getImageUrl(`padges/${statusPadge}.svg`)} alt='status' className='mt-3 h-7' />
 				</div>
 			</div>
 		</Link>
