0de106989e49603d85411092d7fdd4d3d215d5af
mohamed3341 <senior.frontend.two@smartivemedia.com>
2024-04-03 12:08:26 +0200

    handel language

---
+++ src/app/components/CustomHook/LanguageHook.tsx
@@ -0,0 +1,11 @@
+export const UseLanguage = () => {
+
+
+
+    let language: null | undefined | string = undefined
+    if (typeof window !== "undefined") {
+        language = localStorage.getItem("language")
+    }
+
+    return language
+}
