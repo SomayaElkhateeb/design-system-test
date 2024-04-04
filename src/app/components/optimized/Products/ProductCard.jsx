82a12f794421845fe61d61f8d6a15f1466b99fe4
DreamEcho100 <maze6572198@gmail.com>
2024-04-04 13:31:06 +0200

    feat: refactoring 'ProductCard' component

--- src/app/components/optimized/Products/ProductCard.jsx
+++ src/app/components/optimized/Products/ProductCard.jsx
@@ -17,25 +17,53 @@ import {
 	CameraIcon,
 	StarActiveIcon,
 } from 'src/app/utils/icons';
-const ProductCard = ({ id, name, imageUrl, category, options, sku, quantity, price }) => {
+
+/**
+ * @param {{
+ *   id: string;
+ *   name: string;
+ *   imageUrl: string;
+ *   category: string;
+ *   options: number;
+ *   sku: string;
+ *   quantity: number;
+ *   price: string;
+ * }} props
+ *
+ * @example
+ *
+ * ```jsx
+ * <ProductCard
+ *   id='1'
+ *   name='DJI Mavic Pro 2'
+ *   imageUrl='images/Vector.svg'
+ *   category='Blankets'
+ *   options={50}
+ *   sku='123456'
+ *   quantity={50}
+ *   price='10000.00'
+ * />
+ */
+export default function ProductCard(props) {
 	const [isFavorite, setIsFavorite] = useState(false);
 	const [isSelected, setIsSelected] = useState(false);
-	const toggleFavorite = () => {
+
+	function toggleFavorite() {
 		setIsFavorite(!isFavorite);
-	};
-	const isSelectedHandler = () => {
+	}
+	function isSelectedHandler() {
 		setIsSelected(!isSelected);
-	};
+	}
 	const info = {
-		sku: sku,
-		quantity: quantity,
-		price: price,
+		sku: props.sku,
+		quantity: props.quantity,
+		price: props.price,
 	};
-	console.log(info);
+
 	return (
 		<div className='border-2 bg-white overflow-hidden border-light-2 rounded-xl grid grid-cols-1 divide-y p-0 w-[271px] group '>
 			<div className='relative w-full h-[260px]'>
-				<img src={getImageUrl(imageUrl)} alt={name} className='object-cover w-full h-full' />
+				<img src={getImageUrl(props.imageUrl)} alt={props.name} className='object-cover w-full h-full' />
 				<div className='absolute flex flex-col items-center justify-between top-3 bottom-2 left-3'>
 					<div className='flex flex-col items-center gap-4 '>
 						<CheckBox handleOnChange={isSelectedHandler} checked={isSelected} />
@@ -58,81 +86,39 @@ const ProductCard = ({ id, name, imageUrl, category, options, sku, quantity, pri
 			</div>
 			<div className='flex items-end justify-between p-3'>
 				<div className='space-y-1'>
-					<h2 className='title'>{name}</h2>
-					<p className='subtitle'>{category}</p>
-					<p className='paragraph'>{options} Options</p>
+					<h2 className='title'>{props.name}</h2>
+					<p className='subtitle'>{props.category}</p>
+					<p className='paragraph'>{props.options} Options</p>
 				</div>
 				<button>
 					<NextIcon className='fill-subtitle' />
 				</button>
 			</div>
 			{Object.entries(info).map(([posterKey, posterValue]) => (
-				<div className='flex justify-between p-3'>
+				<div className='flex justify-between p-3' key={posterKey}>
 					<p className='subheading'>{posterKey}</p>
 					<p className='paragraph'>{posterValue}</p>
 				</div>
 			))}
 		</div>
 	);
-};
+}
 
-export default ProductCard;
-
-const Actions = () => {
-	<div className='flex flex-col gap-3 px-2 py-1 card'>
-		<button>
-			<ViewIcon className='fill-subtitle' />
-		</button>
-		<button>
-			<EditIcon className='fill-subtitle' />
-		</button>
-		<button>
-			<CopyIcon className='fill-subtitle' />
-		</button>
-		<button>
-			<MoreIcon className='fill-subtitle' />
-		</button>
-	</div>;
-};
-
-ProductCard.defaultProps = {
-	name: 'DJI Mavic Pro 2',
-	imageUrl: 'images/Rectangle.svg',
-	category: 'Blankets',
-	options: 2,
-	sku: 'SF1133569600-1',
-	quantity: 5000,
-	price: 'SAR 100',
-};
-// const ParentComponent = () => {
-//   const products = [
-//     {
-//       id: nanoid(),
-//       name: "DJI Mavic Pro 2",
-//       imageUrl: "images/Rectangle.svg",
-//       category: "Blankets",
-//       options: 2,
-//       sku: "SF1133569600-1",
-//       quantity: 5000,
-//       price: "SAR 100",
-//     },
-//     {
-//       id: nanoid(),
-//       name: "GoPro HERO9 Black",
-//       imageUrl: "images/Rectangle.svg",
-//       category: "Electronics",
-//       options: 1,
-//       sku: "GP2233445500-2",
-//       quantity: 100,
-//       price: "SAR 500",
-//     },
-//   ];
-
-//   return (
-//     <div className="grid grid-cols-3 gap-4">
-//       {products.map((product) => (
-//         <ProductCard key={product.id} {...product} />
-//       ))}
-//     </div>
-//   );
-// };
+function Actions() {
+	return (
+		<div className='flex flex-col gap-3 px-2 py-1 card'>
+			<button>
+				<ViewIcon className='fill-subtitle' />
+			</button>
+			<button>
+				<EditIcon className='fill-subtitle' />
+			</button>
+			<button>
+				<CopyIcon className='fill-subtitle' />
+			</button>
+			<button>
+				<MoreIcon className='fill-subtitle' />
+			</button>
+		</div>
+	);
+}
