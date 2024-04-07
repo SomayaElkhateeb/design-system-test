1fa4a9a9891eb2dd79ca28ad2fa402bc855481cf
DreamEcho100 <maze6572198@gmail.com>
2024-04-07 12:47:52 +0200

    feat: refactoring 'PrevNextBtn' component

--- src/app/components/optimized/UiKits/PrevNextBtn.jsx
+++ src/app/components/optimized/UiKits/PrevNextBtn.jsx
@@ -1,16 +1,14 @@
-import { BackIconSm, NextIconSm } from "src/app/utils/icons";
+import { BackIconSm, NextIconSm } from 'src/app/utils/icons';
 
-const PrevNextBtn = () => {
-  return (
-    <div className="flex w-[67px] h-[34px]">
-      <button className="border rounded-l border-pri-dark w-full flex items-center justify-center">
-        <BackIconSm className=" fill-pri-dark ml-1 mt-1.5" />
-      </button>
-      <button className="border rounded-r border-pri-dark w-full flex items-center justify-center">
-        <NextIconSm className=" fill-pri-dark ml-1.5 mt-1.5" />
-      </button>
-    </div>
-  );
-};
-
-export default PrevNextBtn;
+export default function PrevNextBtn() {
+	return (
+		<div className='flex w-[67px] h-[34px]'>
+			<button className='flex items-center justify-center w-full border rounded-l border-pri-dark'>
+				<BackIconSm className=' fill-pri-dark ml-1 mt-1.5' />
+			</button>
+			<button className='flex items-center justify-center w-full border rounded-r border-pri-dark'>
+				<NextIconSm className=' fill-pri-dark ml-1.5 mt-1.5' />
+			</button>
+		</div>
+	);
+}
