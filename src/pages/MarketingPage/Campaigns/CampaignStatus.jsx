a99cacdd4b616ee83382b988e7f7811a2adbe17d
Ahmed-Pc <>
2024-04-14 11:14:43 +0200

    Campaigns page improvements

--- src/pages/MarketingPage/Campaigns/CampaignStatus.jsx
+++ src/pages/MarketingPage/Campaigns/CampaignStatus.jsx
@@ -1,47 +1,34 @@
 import StatsCard from 'src/app/components/optimized/Cards/StatsCard';
-import { Button, Menu } from 'src/app/components/optimized';
-import { useState } from 'react';
 import data from './data.json';
-import { CalenderIcon, DownIcon } from 'src/app/utils/icons';
-import useSelectBox from 'src/app/components/optimized/Menu/useSelectBox';
-
+import ArrangeButton from 'src/app/components/page/Customers/ArrangeButton.tsx';
+import { useState } from 'react';
+const sortMenus = [
+	{ text: 'Option 1', id: 1 },
+	{ text: 'Option 2', id: 2 },
+	{ text: 'Option 3', id: 3 },
+	{ text: 'Option 4', id: 4 },
+];
 const CampaignStatus = () => {
+	const [selectedOption, setSelectedOption] = useState(null);
+
+	const handleSelect = (option) => {
+		setSelectedOption(option);
+	};
 	return (
-		<>
-			<ComparisonSelector />
+		<div>
+			<div className='mb-4 flex items-center gap-2'>
+				<ArrangeButton sortMenus={sortMenus} selectedOption={selectedOption} handelSelect={handleSelect} />
+				<div className='flex gap-2'>
+					<p className='paragraph text-subtitle'>Compared to:</p>
+					<p className='paragraph text-title'>Last week</p>
+				</div>
+			</div>
 			<div className='flex gap-4 flex-wrap'>
 				{data.statsData.map((item, index) => (
 					<StatsCard key={index} {...item} />
 				))}
 			</div>
-		</>
-	);
-};
-export default CampaignStatus;
-
-const ComparisonSelector = () => {
-	const { isOpen, selectedOption, handleSelect, handleButtonClick } = useSelectBox();
-	const options = [
-		{ text: 'Option 1', id: 1 },
-		{ text: 'Option 2', id: 2 },
-		{ text: 'Option 3', id: 3 },
-		{ text: 'Option 4', id: 4 },
-	];
-
-	return (
-		<div className=' relative flex items-center mb-4'>
-			<Button
-				variant='secondary'
-				LeftIcon={CalenderIcon}
-				RightIcon={DownIcon}
-				text={selectedOption || 'Select'}
-				onClick={handleButtonClick}
-			/>
-			<div className='flex gap-2 ml-3'>
-				<p className='paragraph text-subtitle'>Compared to:</p>
-				<p className='paragraph text-title'>Last week</p>
-			</div>
-			{isOpen && <Menu options={options} selectedOption={selectedOption} onSelect={handleSelect} />}
 		</div>
 	);
 };
+export default CampaignStatus;
