5966269ad540e3e28aa75cd2b75934c7b494e67d
SomayaElkhateeb <frontend.one@smartivemedia.com>
2024-04-29 09:48:40 +0300

    solve conflict

--- src/app/components/optimized/Cards/AppsCard.jsx
+++ src/app/components/optimized/Cards/AppsCard.jsx
@@ -31,16 +31,14 @@ export default function AppsCard(props) {
 
 	const statusPadge = props.status === 'free' ? '' : 'installed';
 	return (
-<<<<<<< HEAD
-		<Link
-			to={props.url}
-			className='flex gap-3 p-3 bg-white border rounded-lg shadow-md border-borders-lines'
-		>
-			<div className='size-[60px] grid place-items-center min-w-[60px] rounded-lg border border-light-2 overflow-hidden'>
-=======
+		// <Link
+		// 	to={props.url}
+		// 	className='flex gap-3 p-3 bg-white border rounded-lg shadow-md border-borders-lines'
+		// >
+		// 	<div className='size-[60px] grid place-items-center min-w-[60px] rounded-lg border border-light-2 overflow-hidden' />
+
 		<Link to={props.url} className='flex gap-3 p-3 bg-white border rounded-lg shadow-md'>
 			<div className='size-[60px]  min-w-[60px] flex items-center'>
->>>>>>> master
 				<img src={props.image} className='object-cover w-full' />
 			</div>
 			<div>
@@ -67,13 +65,11 @@ AppsCard.defaultProps = {
 	label: 'free',
 };
 
-/*
- Usage Example:
-  <AppsCard
-       image="/path/to/image.jpg"
-       name="Telegram"
-       description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-       status="available"
-       url="/app/telegram"
-     />
-*/
+//  Usage Example:
+//   <AppsCard
+//        image="/path/to/image.jpg"
+//        name="Telegram"
+//        description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+//        status="available"
+//        url="/app/telegram"
+//      />
