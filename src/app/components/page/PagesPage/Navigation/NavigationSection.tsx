b28e83edba3cacbbf230fb817e7713ec3f9ddbec
mohamed3341 <medomado201529@gmail.com>
2024-04-30 14:05:42 +0300

    handel pages page

---
+++ src/app/components/page/PagesPage/Navigation/NavigationSection.tsx
@@ -0,0 +1,28 @@
+import { useTranslation } from 'react-i18next';
+import { IoIosAddCircle } from 'react-icons/io';
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'src/app/components/optimized';
+import NavigationCard from './NavigationCard';
+
+export default function NavigationSection() {
+	//  hooks
+	const navigate = useNavigate();
+	const { t } = useTranslation();
+	const array = [...Array(3)];
+	return (
+		<div className='flex-col-top-section-pages'>
+			<div>
+				<Button onClick={() => navigate('/add_page')} variant='primary' LeftIcon={IoIosAddCircle}>
+					{t('Add Page')}
+				</Button>
+			</div>
+			<hr />
+			<div className='flex-col-top-section-pages'>
+                {array?.map((e,i)=>(
+                    <NavigationCard key={i} title='Main menu' sub_title1='About' sub_title2='home' id='1' />
+                ))}
+            </div>
+			
+		</div>
+	);
+}
