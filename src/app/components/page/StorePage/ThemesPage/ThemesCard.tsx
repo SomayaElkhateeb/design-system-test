37082a4021ea55f53f7a8e9f9f0ee1810026d991
mohamed3341 <medomado201529@gmail.com>
2024-05-07 14:14:14 +0300

    handel apps card

--- src/app/components/page/StorePage/ThemesPage/ThemesCard.tsx
+++ src/app/components/page/StorePage/ThemesPage/ThemesCard.tsx
@@ -36,18 +36,10 @@ export default function ThemesCard() {
 				/>
 				<p className='text-[.8rem] font-semibold'>Purfumika</p>
 				<p className='text-[.8rem] text-subtitle'>Perfumes, cosmetics</p>
-				{/* <div className='bg-sec-light w-[3rem] flex flex-row justify-center items-center h-[1.5rem] rounded-[1rem] text-[.8rem] text-sec-hover'>
-					Free
-				</div> */}
-				<div className='w-[8rem] flex justify-start'>
-					{/* <LabelIcon
-					text={"installed"}
-					backgroundColor={"#F3F7FF"}
-					textColor={"#0B47D9"}
-					icon={ <FaCheck size={10} color='#0B47D9' /> }
-				/> */}
-					<Badge status={'installed'} />{' '}
-				</div>
+				
+				
+					<Badge status={'installed'} />
+				
 			</div>
 		</div>
 	);
