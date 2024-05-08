74c35a187765dec5d2fe3e113e9446bf21dbfe07
SomayaElkhateeb <frontend.one@smartivemedia.com>
2024-05-08 19:37:02 +0300

    add page and blog

--- src/app/components/optimized/UiKits/QuickActions.tsx
+++ src/app/components/optimized/UiKits/QuickActions.tsx
@@ -1,20 +1,30 @@
 import { Switch } from '../../ui/switch';
 
-export default function QuickActions() {
+// Define the type for the data array
+type Data = {
+	id: number;
+	title: string;
+};
+
+// Define the type for the props of the QuickActions component
+type QuickActionsProps = {
+	data: Data[];
+};
+
+// Define the QuickActions component with the specified props
+export default function QuickActions({ data }: QuickActionsProps) {
 	return (
 		<div className='global-cards w-72'>
 			<h3 className='text-title font-semibold'>Quick actions</h3>
-			<div className='flex items-center gap-3'>
-				<Switch /> <p>Quick actions</p>
-			</div>
-
-			<div className='flex items-center gap-3'>
-				<Switch /> <p>Quick actions</p>
-			</div>
-
-			<div className='flex items-center gap-3'>
-				<Switch /> <p>Quick actions</p>
-			</div>
+			{data.map((item: Data) => {
+				// Assuming Data is the correct type for each item
+				return (
+					<div className='flex items-center gap-3' key={item.id}>
+						<Switch />
+						<p>{item.title}</p>
+					</div>
+				);
+			})}
 		</div>
 	);
 }
--- src/pages/PagesPage/comp/AddPage.tsx
+++ src/pages/PagesPage/comp/AddPage.tsx
@@ -1,63 +1,104 @@
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
 import AccordionCard from 'src/app/components/optimized/UiKits/AccordionCard';
-import ContentSeo from './ContentSeo';
 import { GroupIcons, HeaderSettings } from 'src/app/components/optimized';
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Form } from 'src/app/components/ui/form';
 import QuickActions from 'src/app/components/optimized/UiKits/QuickActions';
+import { useNavigate } from 'react-router-dom';
+import MainInfoPage from 'src/app/components/page/PagesPage/PagesSection/MainInfoPage';
+import ContentSeoPage from 'src/app/components/page/PagesPage/PagesSection/ContentSeoPage';
+
+export interface addPageInterface {
+	pageTitle: string;
+	link: string;
+	metaKey: string;
+	metaDescription: string;
+	titleEn: string;
+	titleAr: string;
+	descriptionEn: string;
+	descriptionAr: string;
+}
 
 const pageSchema = z.object({
 	pageTitle: z.string().min(3, { message: 'Page title is required' }),
-	link: z.string().refine((value) => /^https?:\/\/(www\.)?link\.net\/[a-zA-Z0-9._]+$/.test(value), {
-		message: 'Invalid URL',
-	}),
+	link: z.string().url(),
 	metaKey: z.string(),
 	metaDescription: z.string().min(7, { message: 'Meta description is required' }),
+	titleEn: z.string().min(3).max(50),
+	titleAr: z.string().min(3).max(50),
+	descriptionEn: z.string().min(10).max(200),
+	descriptionAr: z.string().min(10).max(200),
 });
-
-type TFormInputs = z.infer<typeof pageSchema>;
 
 export default function AddPage() {
 	const { t } = useTranslation();
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<TFormInputs>({
-		mode: 'onBlur',
-		resolver: zodResolver(pageSchema),
-	});
-	const submitForm: SubmitHandler<TFormInputs> = (data) => console.log(data);
+	const navigate = useNavigate();
 
-	const handleSaveChangesClick = () => {
-		handleSubmit(submitForm)();
+	const handleSubmit = (values: addPageInterface) => {
+		console.log(values);
+		// handelclose();
 	};
+
+	const handelDefaultValue = () => {
+		return {
+			pageTitle: '',
+			link: '',
+			metaKey: '',
+			metaDescription: '',
+			titleEn: '',
+			titleAr: '',
+			descriptionEn: '',
+			descriptionAr: '',
+		};
+	};
+
+	const { formStore, onSubmit } = useForm({
+		schema: pageSchema,
+		handleSubmit: handleSubmit,
+		defaultValues: handelDefaultValue(),
+	});
+
+	const data = [
+		{ id: 1, title: 'Visible' },
+		{ id: 2, title: 'Show on footer' },
+		{ id: 3, title: 'Show on main menu' },
+	];
+
 	return (
-		<>
-			<HeaderSettings
-				variant='settingWithIcons'
-				to={-1}
-				title={t('Add page')}
-				groupIcons={<GroupIcons variant='view' />}
-				btn1={{ text: t('Discard'), onClick: () => {} }}
-				btn2={{
-					text: t('Save Changes'),
-					onClick: handleSaveChangesClick,
-				}}
-			/>
-			<div className='p-4 flex gap-7 w-full justify-between'>
-				<div className=' gap-7 flex flex-col w-full'>
-					<AccordionCard
-						content={<ContentSeo register={register} errors={errors} />}
-						title='SEO (Search engine listing preview)'
+		<div>
+			<Form {...formStore}>
+				<form className='flex-col-top-section-pages gap-[1.7rem]' onSubmit={onSubmit}>
+					<HeaderSettings
+						variant='settingWithIcons'
+						to={-1}
+						title={t('Add page')}
+						groupIcons={<GroupIcons variant='view' />}
+						btn1={{
+							text: t('Discard'),
+							onClick: () => {
+								navigate(-1);
+							},
+						}}
+						btn2={{
+							text: t('Save Changes'),
+							onClick: () => {},
+						}}
 					/>
-				</div>
-				<div>
-					<QuickActions />
-				</div>
-			</div>
-		</>
+					<div className='p-4 flex gap-7 w-full justify-between'>
+						<div className=' gap-7 flex flex-col w-full'>
+							{/* <MainInfoPage formStore={formStore} /> */}
+							<AccordionCard
+								content={<ContentSeoPage formStore={formStore} />}
+								title={t('SEO (Search engine listing preview)')}
+							/>
+						</div>
+						<div>
+							<QuickActions data={data} />
+						</div>
+					</div>
+				</form>
+			</Form>
+		</div>
 	);
 }
