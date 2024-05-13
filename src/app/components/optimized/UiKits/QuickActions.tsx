6ab01cf4e227eb6226e53220d3b7c2ae57578ddc
mohamed3341 <medomado201529@gmail.com>
2024-05-13 13:00:45 +0300

    handel branches page

--- src/app/components/optimized/UiKits/QuickActions.tsx
+++ src/app/components/optimized/UiKits/QuickActions.tsx
@@ -1,16 +1,17 @@
 import { Switch } from '../../ui/switch';
 import { useTranslation } from 'react-i18next';
 
-export default function QuickActions() {
+export default function QuickActions({ data }: { data: { id: number; title: string }[] }) {
 	const { t } = useTranslation();
 	return (
 		<div className='serviceDetails-sharedClass p-5 flex-col-top-section-pages'>
 			<h3 className='title'>{t('Quick actions')}</h3>
-
-			<div className='flex items-center gap-3'>
-				<Switch />
-				<p>{t('Activated')}</p>
-			</div>
+			{data?.map((e) => (
+				<div key={e.id} className='flex items-center gap-3'>
+					<Switch />
+					<p>{e.title}</p>
+				</div>
+			))}
 		</div>
 	);
 }
