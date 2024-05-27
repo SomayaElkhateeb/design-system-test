f2648ae289909e116354208d33f39cc2fa212fd2
DreamEcho100 <maze6572198@gmail.com>
2024-05-27 15:16:43 +0300

    minor change

--- src/app/utils/hooks/form.ts
+++ src/app/utils/hooks/form.ts
@@ -3,19 +3,19 @@ import { ZodObject, ZodRawShape, z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { UseFormProps, useForm as useRHForm } from 'react-hook-form';
 
-export type InferredZodSchema<T extends ZodObject<ZodRawShape> | ZodRawShape> =
-	T extends ZodRawShape
-		? z.infer<z.ZodObject<T>>
-		: T extends ZodObject<ZodRawShape>
-		? z.infer<T>
+export type InferredZodSchema<TZodShape extends ZodObject<ZodRawShape> | ZodRawShape> =
+	TZodShape extends ZodRawShape
+		? z.infer<z.ZodObject<TZodShape>>
+		: TZodShape extends ZodObject<ZodRawShape>
+		? z.infer<TZodShape>
 		: never;
 
-export function useForm<TZodRawShape extends ZodObject<ZodRawShape> | ZodRawShape>(params: {
-	schema: TZodRawShape;
-	defaultValues: UseFormProps<InferredZodSchema<TZodRawShape>>['defaultValues'];
-	handleSubmit: (validatedData: InferredZodSchema<TZodRawShape>) => void;
+export function useForm<TZodShape extends ZodObject<ZodRawShape> | ZodRawShape>(params: {
+	schema: TZodShape;
+	defaultValues: UseFormProps<InferredZodSchema<TZodShape>>['defaultValues'];
+	handleSubmit: (validatedData: InferredZodSchema<TZodShape>) => void;
 }) {
-	const formStore = useRHForm<InferredZodSchema<TZodRawShape>>({
+	const formStore = useRHForm<InferredZodSchema<TZodShape>>({
 		resolver: zodResolver(
 			// @ts-ignore
 			'shape' in params.schema ? params.schema : z.object(params.schema),
