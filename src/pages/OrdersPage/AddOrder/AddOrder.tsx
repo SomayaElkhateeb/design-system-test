3bc231ce41c7522d139a19c65719b110aaf5288a
frontend003 <162417752+frontend003@users.noreply.github.com>
2024-06-06 19:45:52 +0300

    Fix TabX

--- src/pages/OrdersPage/AddOrder/AddOrder.tsx
+++ src/pages/OrdersPage/AddOrder/AddOrder.tsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { HeaderSettings, VerticalTabs } from 'src/app/components/optimized';
+import { HeaderSettings, TabX } from 'src/app/components/optimized';
 import { AddCheckout, Address, Customer, Products } from 'src/app/components/page';
 
 export default function AddOrder() {
 	const { t } = useTranslation();
+
+	const [currentTab, setCurrentTab] = useState(0);
+	const [_, setFinish] = useState(false);
+
+	const handleTabClick = (index: number) => {
+		setCurrentTab(index);
+	};
+
+	const handleNext = () => {
+		if (currentTab < tabs.length - 1) {
+			setCurrentTab(currentTab + 1);
+		}
+	};
+
+	const handlePrev = () => {
+		if (currentTab > 0) {
+			setCurrentTab(currentTab - 1);
+		}
+	};
+
+	const handleFinish = (value: boolean) => {
+		setFinish(value);
+	};
 
 	const tabs = [
 		{
@@ -27,7 +51,14 @@ export default function AddOrder() {
 		<>
 			<HeaderSettings title={t('add new order')} submit />
 			<div className='custom_container mx-0 py-5 lg:w-3/4 sm:px-1'>
-				<VerticalTabs tabs={tabs} />
+				<TabX
+					tabs={tabs}
+					currentTab={currentTab}
+					handleNext={handleNext}
+					handlePrev={handlePrev}
+					handleFinish={handleFinish}
+					handleTabClick={handleTabClick}
+				/>
 			</div>
 		</>
 	);
