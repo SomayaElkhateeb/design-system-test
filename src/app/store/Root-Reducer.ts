1b385df64f9ca65c83ae372805f847169492be48
frontend003 <162417752+frontend003@users.noreply.github.com>
2024-06-22 12:21:44 +0300

    fix errors

--- src/app/store/Root-Reducer.ts
+++ src/app/store/Root-Reducer.ts
@@ -1,10 +1,6 @@
-import { combineReducers } from "@reduxjs/toolkit";
+import { combineReducers } from '@reduxjs/toolkit';
 import discount from './slices/marketing/discounts/discountSlice';
 import coupons from './slices/marketing/coupons/couponSlice';
-import categories from './slices/marketing/categories/categoriesSlice';
-import customers from './slices/marketing/customers/customersSlice';
-import products from './slices/marketing/products/productsSlice';
-import groups from './slices/marketing/groups/groupsSlice';
 import productsAnalytics from './slices/analyticsPage/ProductsAnalytics/productsAnalyticsTableSlice';
 import ordersAnalytics from './slices/analyticsPage/OrderAnalytics/orderAnalyticsTableSlice';
 import customersAnalytics from './slices/analyticsPage/CustomerAnalytics/customersAnalyticsTableSlice';
@@ -15,25 +11,30 @@ import blog from './slices/pagesPage/blog/blogTableSlice';
 import allProducts from './slices/productsPage/allProducts/allProductsTableSlice';
 import brands from './slices/productsPage/brands/brandsTableSlice';
 import inventory from './slices/productsPage/inventory/inventoryTableSlice';
+// ===========================
+import categoriesSlice from './slices/selectors/categories/categoriesSlice';
+import customersSlice from './slices/selectors/customers/customersSlice';
+import productsSlice from './slices/selectors/products/productsSlice';
+import groupsSlice from './slices/selectors/groups/groupsSlice';
 export const rootReducer = combineReducers({
-    discount: discount, //
-    coupons: coupons, //
-    categories: categories,
-    customers: customers,
-    products: products,
-    groups: groups,
-    // analytics page
-    productsAnalytics: productsAnalytics, //
-    ordersAnalytics: ordersAnalytics, //
-    customersAnalytics: customersAnalytics, //
-    // customers page
-    allCustomer: allCustomer, //
-    customersGroup: customersGroup, //
-    // pages page
-    pages: pages, //
-    blog: blog, //
-    // products page
-    allProducts: allProducts, //
-    brands: brands, //
-    inventory: inventory, //
-})
+	discount: discount, //
+	coupons: coupons, //
+	categories: categoriesSlice,
+	customers: customersSlice,
+	products: productsSlice,
+	groups: groupsSlice,
+	// analytics page
+	productsAnalytics: productsAnalytics, //
+	ordersAnalytics: ordersAnalytics, //
+	customersAnalytics: customersAnalytics, //
+	// customers page
+	allCustomer: allCustomer, //
+	customersGroup: customersGroup, //
+	// pages page
+	pages: pages, //
+	blog: blog, //
+	// products page
+	allProducts: allProducts, //
+	brands: brands, //
+	inventory: inventory, //
+});
