30b1c3dc401988f4fc3c77d640a303029f8a8f1e
SomayaElkhateeb <frontend.one@smartivemedia.com>
2024-07-01 08:41:10 +0300

    ..

--- src/app/components/optimized/Buttons/ProfileBtn.tsx
+++ src/app/components/optimized/Buttons/ProfileBtn.tsx
@@ -1,29 +1,21 @@
 import { getImageUrl } from 'src/app/utils';
-
 import ManageAccountCard from '../Cards/ManageAccountCard';
 import PopoverComponenet from '../Popover/Popover';
 import { useState } from 'react';
 
-/**
- * ProfileBtn component represents a button that, when clicked, displays a user's profile image
- * and opens the manage account card when clicked.
- */
 const ProfileBtn = () => {
-	const [show, setShow] = useState(false);
+	const [show, setShow] = useState(true);
+
 	return (
 		<PopoverComponenet
 			close={() => setShow(show)}
 			button={
 				<div className='roundedParentIcon'>
-					<img
-						src={getImageUrl('images/profile.png')}
-						alt='logo'
-						className=' object-cover h-full '
-					/>
+					<img src={getImageUrl('images/profile.png')} alt='logo' className='object-cover h-full' />
 				</div>
 			}
 		>
-			<ManageAccountCard onClose={() => setShow(true)} />
+			<ManageAccountCard onClose={() => setShow(false)} />
 		</PopoverComponenet>
 	);
 };
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -11,7 +11,6 @@ import ProductHighlights from './_comp/ProductHighlights';
 import Setups from './_comp/Setups';
 import data from './_comp/data.json';
 import { useHomePage } from './_comp/useHomePage';
-import { ChooseEmail } from '..';
 
 // --------------------------------------------------------------------------
 // import { lazy, Suspense } from 'react';
@@ -45,7 +44,7 @@ import { ChooseEmail } from '..';
 // import { useHomePage } from './_comp/useHomePage';
 // --------------------------------------------------------------------------
 
-const slides = [
+export const slides = [
 	{
 		videoUrl: 'https://www.youtube.com/embed/pUb9EW770d0?si=JMDd8iQlC39CoyXa',
 		title: 'How to finish steps',
@@ -80,8 +79,7 @@ export default function HomePage() {
 	if (showLoading) {
 		return <HomeLoading />;
 	}
-	// <Suspense fallback={<HomeLoading />}>
-	return <ChooseEmail />;
+
 	return (
 		<div className='custom_container grid grid-cols-1 gap-5 pt-5'>
 			<div className='custom-grid-parent'>
