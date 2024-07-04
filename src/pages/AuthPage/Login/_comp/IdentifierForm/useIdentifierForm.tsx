58069a4402c4067b8134b506efae5562259a593d
mohamed3341 <senior.frontend.two@smartivemedia.com>
2024-07-04 21:53:12 +0300

    handel auth input

--- src/pages/AuthPage/Login/_comp/IdentifierForm/useIdentifierForm.tsx
+++ src/pages/AuthPage/Login/_comp/IdentifierForm/useIdentifierForm.tsx
@@ -12,23 +12,18 @@ export interface IdentifierFormProps {
 	setEmail: (e: string) => void;
 }
 const schemas = {
-	email: z.string().min(1, { message: 'Email is required' }).email('Invalid email address'),
-	phone: z.string().min(7, { message: 'Phone is required' }),
+	emailOrPhone: z.string().min(1, { message: 'Input is required' }),
 };
-
-const getSchema = (inputType: InputType) => ({
-	emailOrPhone: schemas[inputType],
-});
 
 export default function useIdentifierForm({
 	setStep,
 	onIdentifierChange,
 	setEmail,
 }: IdentifierFormProps) {
-	const [inputType, setInputType] = useState<InputType>(InputType.Email);
-	console.log(schemas[inputType]);
+	
+	
 	const { formStore, onSubmit } = useForm({
-		schema: getSchema(inputType),
+		schema: schemas,
 		handleSubmit: (values: { emailOrPhone: string }) => {
 			setStep(2);
 			setEmail(values.emailOrPhone);
@@ -36,13 +31,7 @@ export default function useIdentifierForm({
 		defaultValues: { emailOrPhone: '' },
 	});
 
-	const handleTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const value = event.target.value.trim();
-		let newType: InputType = /^\d+$/.test(value) ? InputType.Phone : InputType.Email;
-		console.log(value);
-		console.log(newType);
-		setInputType(newType);
-	};
+	
 
 	useEffect(() => {
 		const subscription = formStore.watch((value) => {
@@ -52,5 +41,5 @@ export default function useIdentifierForm({
 		return () => subscription.unsubscribe();
 	}, [formStore.watch('emailOrPhone'), onIdentifierChange]);
 
-	return { formStore, onSubmit, handleTypeChange };
+	return { formStore, onSubmit };
 }
