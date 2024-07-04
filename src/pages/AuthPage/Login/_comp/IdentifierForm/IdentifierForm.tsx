8cd7ac2495bc0df68c5360b5d050b9adc8edd7dd
SomayaElkhateeb <frontend.one@smartivemedia.com>
2024-07-04 15:21:08 +0300

    ..

--- src/pages/AuthPage/Login/_comp/IdentifierForm/IdentifierForm.tsx
+++ src/pages/AuthPage/Login/_comp/IdentifierForm/IdentifierForm.tsx
@@ -1,15 +1,10 @@
 import { Link } from 'react-router-dom';
-
 import { Form } from 'src/app/components/ui/form';
 import { Input } from 'src/app/components/ui/input';
 import { Button } from 'src/app/components/optimized';
 import FormField from 'src/app/components/ui/form/field';
 import useIdentifierForm, { IdentifierFormProps } from './useIdentifierForm';
 import { useTranslation } from 'react-i18next';
-import { AuthApi } from 'src/app/React-Query/authApi';
-import { useMutation } from 'react-query';
-import PublicHandelingErrors from 'src/app/utils/AxiosUtils/PublicHandelingErrors';
-import toast from 'react-hot-toast';
 
 export default function IdentifierForm({ setStep, onIdentifierChange }: IdentifierFormProps) {
 	const { formStore, onSubmit, handleTypeChange } = useIdentifierForm({
@@ -17,18 +12,6 @@ export default function IdentifierForm({ setStep, onIdentifierChange }: Identifi
 		onIdentifierChange,
 	});
 	const { t } = useTranslation();
-
-	const { mutate } = useMutation('login', AuthApi.login, {
-		onSuccess: async (response) => {
-			toast.success(response?.data?.message);
-			console.log(response?.data?.data.token);
-		},
-		onError: PublicHandelingErrors.onErrorResponse,
-	});
-
-	const handleSignIn = (values: { emailOrPhone: string }) => {
-		mutate({ loginData: values.emailOrPhone });
-	};
 
 	return (
 		<Form {...formStore}>
@@ -53,13 +36,7 @@ export default function IdentifierForm({ setStep, onIdentifierChange }: Identifi
 					</Link>
 				</div>
 				<div className='flex justify-end mb-11'>
-					<Button
-						type='submit'
-						variant='primary'
-						text={t('Next')}
-						className='w-36'
-						omClick={handleSignIn}
-					/>
+					<Button type='submit' variant='primary' text={t('Next')} className='w-36' />
 				</div>
 				<p className='paragraph text-subtitle text-center'>
 					{t("Don't have an account?")}&nbsp;
--- src/pages/AuthPage/Login/_comp/PasswordForm/usePasswordForm.tsx
+++ src/pages/AuthPage/Login/_comp/PasswordForm/usePasswordForm.tsx
@@ -2,6 +2,10 @@ import { useCallback, useState } from 'react';
 import { z } from 'zod';
 
 import { useForm } from 'src/app/utils/hooks/form';
+import toast from 'react-hot-toast';
+import { AuthApi } from 'src/app/React-Query/authApi';
+import { useMutation } from 'react-query';
+import PublicHandelingErrors from 'src/app/utils/AxiosUtils/PublicHandelingErrors';
 
 const passwordSchema = {
 	password: z.string().min(8, 'Password must be at least 8 characters long'),
@@ -10,11 +14,32 @@ const passwordSchema = {
 export default function usePasswordForm() {
 	const [isVisible, setIsVisible] = useState(false);
 
+	const { mutate } = useMutation('login', AuthApi.login, {
+		onSuccess: async (response) => {
+			console.log(response?.data);
+			toast.success(response?.data?.message);
+			// set to local storage
+			// localStorage.setItem('token_login', response?.data?.data?.token);
+			// localStorage.setItem('domain_login', response?.data?.data?.data?.company?.domain);
+		},
+		onError: PublicHandelingErrors.onErrorResponse,
+	});
+
+	const handleSubmit = (values: { password: string }) => {
+		mutate(values);
+	};
+
 	const { formStore, onSubmit } = useForm({
 		schema: passwordSchema,
-		handleSubmit: (values: { password: string }) => console.log(values),
+		handleSubmit: handleSubmit,
 		defaultValues: { password: '' },
 	});
+	///////////////////////////////////
+	// const { formStore, onSubmit } = useForm({
+	// 	schema: passwordSchema,
+	// 	handleSubmit: (values: { password: string }) => console.log(values),
+	// 	defaultValues: { password: '' },
+	// });
 	const toggleVisibility = useCallback(() => {
 		setIsVisible((prev) => !prev);
 	}, []);
