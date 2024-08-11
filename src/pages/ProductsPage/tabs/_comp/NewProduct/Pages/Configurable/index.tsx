20153ee4b25196122038cdd46d77f9b765b89c30
mohamed3341 <medomado201529@gmail.com>
2024-08-11 09:59:27 +0300

    handel project

--- src/pages/ProductsPage/tabs/_comp/NewProduct/Pages/Configurable/index.tsx
+++ src/pages/ProductsPage/tabs/_comp/NewProduct/Pages/Configurable/index.tsx
@@ -21,6 +21,8 @@ import { ProductFormValues } from './types';
 import { useAppDispatch } from 'src/app/store';
 import { useEffect, useMemo } from 'react';
 import { getInventoryTable } from 'src/app/store/slices/productsPage/inventory/inventoryAsyncThunks';
+import { PostSimpleQuickProduct } from 'src/app/store/slices/productsPage/allProducts/allProductsAsyncThunks';
+import { useNavigate } from 'react-router-dom';
 const productsSections = [
 	// {
 	// 	Elem: ProductFormMediaSection,
@@ -72,28 +74,60 @@ const productsSections = [
 export default function ConfigurableProductPage() {
 	const { t } = useTranslation();
 	const dispatch = useAppDispatch()
+	const navigate =useNavigate()
 	const { formStore, onSubmit } = useForm({
 		schema: ProductSchema,
 		handleSubmit: (values) => {
-
+			//  handel inventory of product
 			let handelInventory = values.inventories?.map((el: any, i) => {
 				return {
 					[`inventories[${el.id}]`]: values?.quy?.toString(),
 				};
 			});
+			//  convert array inventory to object
 			const obj = handelInventory.reduce((acc: any, item: any) => {
 				const key = Object.keys(item)[0];
 				acc[key] = item[key];
 				return acc;
 			}, {});
 
+			let variants = values.variants?.map((e: any) => {
+				//  handel inventory of variants
+				let handelInventoryVariants = e?.inventories?.map((el: any, i) => {
+					return {
+						[`inventories[${el.id}]`]: e?.quantity?.toString(),
+					};
+				});
+				//  convert array inventory of variants to object
+				const InventoryVariantsObj = handelInventoryVariants.reduce((acc: any, item: any) => {
+					const key = Object.keys(item)[0];
+					acc[key] = item[key];
+					return acc;
+				}, {});
+				return {
+					...e,
+					[e.code]: e.attributeValues,
+					...InventoryVariantsObj,
+				};
+			});
+
+			const variantsData = variants.reduce((acc: any, variant: any, index: number) => {
+				acc[`variant_${index}`] = variant;
+				return acc;
+			}, {});
 
 			let refactorData = {
-				...values, 'categories[]': values.category, ["en[name]"]: values.nameEn,
+				...values, "type": "configurable", 'categories[]': values.category, ["en[name]"]: values.nameEn,
 				["ar[name]"]: values.nameAr, ["en[description]"]: values.descriptionEn,
-				["ar[description]"]: values.descriptionAr, ...obj
+				["ar[description]"]: values.descriptionAr, ...obj,
+				variants: variantsData,inventories:values.inventories?.map((e)=>e.id)
 			}
-			console.log(refactorData)
+			
+			dispatch(PostSimpleQuickProduct(refactorData)).then((promiseResponse) => {
+				if ((promiseResponse.payload.code = 200)) {
+					navigate(-1);
+				}
+		  })
 		},
 		defaultValues: ProductDefaultValues,
 	});
